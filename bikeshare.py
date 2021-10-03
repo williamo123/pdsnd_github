8afce9b1686facf2958e1f9664a6af43494a72d4
William Orona <williamo123@gmail.com>
2021-10-02 17:06:42 -0700

    refactor: added more descriptive introduction

--- bikeshare.py
+++ bikeshare.py
@@ -15,7 +15,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! This program will allow you to choose a city, month, day of week, and hour to display bikeshare data previously collected.')
     # Get user input for city (chicago, new york city, washington).
 
     city = ''
